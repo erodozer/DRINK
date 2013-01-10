fa1c4919537eb95d1c63d4bcede231f7433fdef1
Nicholas Hydock <nhydock@gmail.com>
2013-01-10 06:52:51 -0500

    Allow SpriteSheet to use texture regions instead of a whole Texture

--- src/sugdk/graphics/SpriteSheet.java
+++ src/sugdk/graphics/SpriteSheet.java
@@ -14,17 +14,34 @@ public class SpriteSheet
 	TextureRegion[][] rows;
 	TextureRegion[][] columns;
 	
+	int xFrames;
+	int yFrames;
+	private int[] frameSize;
+	
 	/**
 	 * Creates a spritesheet from a texture
+	 * @param tex
+	 * @param xFrames
+	 * @param yFrames
+	 */
+	public SpriteSheet(Texture tex, int xFrames, int yFrames)
+	{
+		this(new TextureRegion(tex), xFrames, yFrames);
+	}
+	
+	/**
+	 * Creates a spritesheet from a texture region
 	 * @param tex - texture file
 	 * @param xFrames - number of horizontal frames in the spritesheet
 	 * @param yFrames - number of vertical frames in the spritesheet
 	 */
-	public SpriteSheet(Texture tex, int xFrames, int yFrames)
+	public SpriteSheet(TextureRegion tex, int xFrames, int yFrames)
 	{
+		this.xFrames = xFrames;
+		this.yFrames = yFrames;
 		rows = new TextureRegion[yFrames][xFrames];
 		columns = new TextureRegion[xFrames][yFrames];
-		int[] frameSize = {tex.getWidth()/xFrames, tex.getHeight()/yFrames};
+		frameSize = new int[]{tex.getRegionWidth()/xFrames, tex.getRegionHeight()/yFrames};
 		for (int y = 0; y < yFrames; y++)
 		{
 			for (int x = 0; x < xFrames; x++)
@@ -65,5 +82,33 @@ public class SpriteSheet
 	public TextureRegion getFrame(int x, int y)
 	{
 		return columns[x][y];
+	}
+	
+	/**
+	 * Pick a single frame as if the Spritesheet is along one long row/col
+	 * @param frame - frame index in the spritesheet (0...n-1)
+	 * @param indexDirection - true for down rows then next column, false for across columns then next row
+	 * @return a region of the spritesheet
+	 */
+	public TextureRegion getFrame(int frame, boolean indexDirection)
+	{
+		if (indexDirection)
+		{
+			return columns[frame/yFrames][frame%yFrames];
+		}
+		else
+		{
+			return rows[frame/xFrames][frame%xFrames];
+		}
+	}
+	
+	public int getFrameWidth()
+	{
+		return frameSize[0];
+	}
+	
+	public int getFrameHeight()
+	{
+		return frameSize[1];
 	}
 }
